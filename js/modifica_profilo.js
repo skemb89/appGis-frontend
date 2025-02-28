921ad6fe791edfc26d36ae5e86e992b5ab3b124a
skemb89 <skemb89@gmail.com>
2025-02-28 14:21:28 +0100

    Modifica profilo

---
+++ js/modifica_profilo.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    // Recuperiamo il token JWT dal localStorage
+    const token = localStorage.getItem('token');
+  
+    if (!token) {
+      // Se non c'è il token, redirigi alla pagina di login
+      window.location.href = 'login.html';
+      return;
+    }
+  
+    try {
+      // Eseguiamo la richiesta per ottenere il profilo dell'utente
+      const response = await fetch('/api/auth/profile2', {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`, // Aggiungi il token nella richiesta
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error('Errore nel recuperare il profilo');
+      }
+  
+      const data = await response.json();
+  
+      // Mostra le informazioni dell'utente nel form
+      document.getElementById('current-username').textContent = data.username;
+      document.getElementById('current-giocatore').textContent = data.giocatore;
+  
+    } catch (error) {
+      console.error('Errore nel recuperare il profilo:', error);
+      // Mostra un messaggio di errore se la richiesta non va a buon fine
+      alert('Errore nel recuperare il profilo');
+    }
+  });
+  
